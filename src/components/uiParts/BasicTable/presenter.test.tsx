a74d4ac5cd45574279f2c5559a060d75b5394ef1
norinori777 <norinori6791@gmail.com>
2024-10-07 00:30:09 +0900

    Tableのwarning改善対応としてのindex追加とテストコード追加

---
+++ src/components/uiParts/BasicTable/presenter.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom';
+import { BasicTable } from './presenter'
+
+interface TestItem {
+    id: number
+    name: string
+    active: string
+    [key: string]: string | number | boolean
+}
+
+describe('BasicTable', () => {
+    test('データがない場合、 "データがありません。" が表示されること', () => {
+        render(<BasicTable<TestItem> titleHeader={['ID', 'Name', 'Active']} items={null} />)
+        expect(screen.getByText('データがありません。')).toBeInTheDocument()
+    })
+
+    test('itemsの数だけrowが表示されること', () => {
+        const items: TestItem[] = [
+            { id: 1, name: 'Item 1', active: 'true' },
+            { id: 2, name: 'Item 2', active: 'false' },
+        ]
+        render(<BasicTable<TestItem> titleHeader={['id', 'name', 'active']} items={items} />)
+        expect(screen.getAllByRole('row')).toHaveLength(3)
+    })
+
+    test('表のヘッダーとデータが正しく表示されること', () => {
+        const items: TestItem[] = [
+            { id: 1, name: 'Item 1', active: 'true' },
+            { id: 2, name: 'Item 2', active: 'false' },
+        ]
+        render(<BasicTable<TestItem> titleHeader={['id', 'name', 'active']} items={items} />)
+
+        // Check headers
+        expect(screen.getByText('id')).toBeInTheDocument()
+        expect(screen.getByText('name')).toBeInTheDocument()
+        expect(screen.getByText('active')).toBeInTheDocument()
+
+        // Check data
+        expect(screen.getByText('1')).toBeInTheDocument()
+        expect(screen.getByText('Item 1')).toBeInTheDocument()
+        expect(screen.getByText('true')).toBeInTheDocument()
+        expect(screen.getByText('2')).toBeInTheDocument()
+        expect(screen.getByText('Item 2')).toBeInTheDocument()
+        expect(screen.getByText('false')).toBeInTheDocument()
+
+        expect(screen.getAllByRole('row')).toHaveLength(3)
+    })
+})
