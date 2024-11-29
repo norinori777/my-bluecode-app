4fed390a9acaebb68164bc66179b2a09a015cfda
norinori777 <norinori6791@gmail.com>
2024-11-30 03:13:07 +0900

    setValueのメソッドの受け取るように修正。また、カレンダーICON追加

--- src/components/pages/MemberList/components/SearchMemberForm/validators.ts
+++ src/components/pages/MemberList/components/SearchMemberForm/validators.ts
@@ -6,11 +6,9 @@ export const schema = () => yup.object({
         .max(10, '検索ワードは、10文字となります。'),
     startDate: yup
         .date()
-        .nullable()
         .required('開始日付は必須です。'),
     endDate: yup
         .date()
-        .nullable()
         .required('終了日付は必須です。')
-        .min(yup.ref('startDate'), '終了日付は開始日付より後の日付を選択してください。'),
+        .min(yup.ref('startDate'), '終了日付は開始日付より後の日付を選択してください。')
 })
